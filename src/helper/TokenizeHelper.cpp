da84a34ba914a6a59622ab2358680adf93753773
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-11-11 22:50:06 +0100

    add boilerplate classes

---
+++ src/helper/TokenizeHelper.cpp
@@ -0,0 +1,12 @@
+#include "TokenizeHelper.h"
+
+#include "structs/TokenizeAttempt.h"
+
+static TokenizeAttempt tokenizeStringIdentifiers(std::string code) {
+    return TokenizeAttempt();
+}
+
+static TokenizeAttempt tokenizeKeywordPunctuators(std::string code) {
+    return TokenizeAttempt();
+}
+
---
+++ src/helper/TokenizeHelper.h
@@ -0,0 +1,16 @@
+
+#ifndef COMPILER_LAB_TOKENIZEHELPER_H
+#define COMPILER_LAB_TOKENIZEHELPER_H
+#include <string>
+
+#include "structs/TokenizeAttempt.h"
+
+
+class TokenizeHelper {
+public:
+    static TokenizeAttempt tokenizeStringIdentifiers(std::string identString);
+    static TokenizeAttempt tokenizeKeywordPunctuators(std::string punctString);
+};
+
+
+#endif //COMPILER_LAB_TOKENIZEHELPER_H
