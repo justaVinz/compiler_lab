4008cad87a9956bb5934e3157e6383364c1a8386
vinzenz.brehme <vinzenz.brehme@bayard-consulting.com>
2025-11-11 22:38:14 +0100

    update main with sample token and attempt

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -2,15 +2,19 @@
 #include <fstream>
 #include <string>
 
+#include "../helper/structs/TokenizeAttempt.h"
 #include "./../helper/structs/Token.h"
 
 #define TEST_PATH "./test/lexer/test_1.c"
 
 
 int main() {
-    std::string line = "if (x > 0) { for } else";
-    // static std::string sourceCode = Utils::readSourceCode(TEST_PATH);
+    std::cout << "Start" << std::endl;
     Token token("IDENTIFIER", "var", 3, 3);
+    std::cout << "Token created" << std::endl;
     std::cout << token << std::endl;
-    return 0;
+    TokenizeAttempt attempt(token, 3);
+    std::cout << "TokenizeAttempt created" << std::endl;
+    std::cout << attempt << std::endl;
+    std::cout << "Token output done" << std::endl;
 }
